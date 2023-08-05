922af60da416bfeed6e2e9201db0dfbd920d2722
Singgih Pratama <singgih.vendor.pasti@pegadaian.co.id>
2023-08-05 13:24:30 +0700

    initialize

---
+++ src/main/java/teatech/organicjava/OrganicJavaApplication.java
@@ -0,0 +1,13 @@
+package teatech.organicjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganicJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganicJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/teatech/organicjava/OrganicJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package teatech.organicjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganicJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
